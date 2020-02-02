3ff868a92d861482a6f0bdeaad25675870b64945
codelieche <codelieche@gmail.com>
2020-02-02 21:58:58 +0800

    Fron Add  UploadImageItem Hook

---
+++ front/src/components/Hooks/UplaodImageItem.js
@@ -0,0 +1,102 @@
+/**
+ * Form组件上传图片的Item:
+ * 需要传递的属性：展示图片的url
+ * - url: 显示略缩图的url
+ * - fileListData: 文件列表
+ * - fileListDataState: 操作文件列表状态的函数
+ * 
+ *  使用示例：
+ *      const [fileListData, fileListDataState] = useState([]);
+ *      {useUploadImageItem(data.image, fileListData, fileListDataState)}
+ * 在上级函数中操作fileListData即可
+ */
+
+import React, { useState, useEffect} from "react";
+
+import {
+    Upload, message
+} from "antd";
+
+// 图片上传Item
+// 参数：
+// 1. url: 图片的url，可为空
+// 2. fileListData: 文件列表
+// 3. fileListDataState: 操作文件列表的state函数
+function useUploadImageItem(url, fileListData, fileListDataState){
+
+    // url状态
+    const [isUploaded, isUploadedState] = useState(false);
+    const [imageUrl, imageUrlState] = useState(url);
+
+    // 相当于：componentDidMount等函数
+    useEffect(() => {
+        // console.log(url, imageUrl);
+        // console.log(url !== imageUrl, !isUploaded)
+        if(url !== imageUrl && !isUploaded){
+            imageUrlState(url);
+        }
+    }, [url, imageUrl, isUploaded]);
+
+    // 上传组件属性
+    const uploadProps = {
+        name: "file",
+        multiple: false,
+        onRemove: (file) => {
+            // 把feilename字段设置为空
+            console.log(file);
+            fileListDataState(prevState => {
+                let fileList = prevState;
+                // 把当前的file从fileList中移除
+                const index = fileList.indexOf(file);
+                if(index >= 0 && imageUrl){
+                    imageUrlState(null);
+                }
+                const newFileList = fileList.slice();
+                newFileList.splice(index, 1);
+
+                return newFileList;
+            });
+        },
+
+        beforeUpload: (file) => {
+            // console.log(file);
+            // 选择了文件后，把文件名设置到form中
+            if(file.type.indexOf("image") < 0){
+                message.warn("请选择图片文件");
+                // console.log(file, file.type);
+                return false;
+            }
+            
+            // 修改文件列表的数据
+            fileListDataState([file]);
+
+            let uploadImageUrl = URL.createObjectURL(file);
+            isUploadedState(true);
+            imageUrlState(uploadImageUrl);
+            return false;
+        },
+        fileList: fileListData,
+    }
+    // 显示略缩图
+    let imageElement;
+    if(imageUrl){
+        imageElement = (
+          <img style={{maxWidth: "100%", height: "auto"}}  
+            src={imageUrl} alt="图片" />
+      );
+    }
+
+    return(
+        <Upload.Dragger
+          {...uploadProps}
+        >
+            <p className="ant-upload-hint">
+                请选择一张图片
+                {imageElement}
+            </p>
+        </Upload.Dragger>
+    );
+
+}
+
+export default useUploadImageItem;
---
+++ front/src/components/Task/Category/Form.js
@@ -0,0 +1,176 @@
+/**
+ * 分类 表单Form
+ * 需要传递的属性(props):
+ * 1. data: 编辑页会传递过来object对象
+ * 2. type: editor或者add，根据这个类确定GroupForm
+ * 3. handleSubbmit: 表单提交时候，需要做的事情
+ */
+import React, { useState, useEffect } from "react";
+import {
+    Form,
+    Input,
+    InputNumber,
+    Row,
+    Col,
+    Button,
+    message
+} from "antd";
+
+// 分类表单
+function CategoryForm(props){
+    // 表单的ref
+    const formRef = React.createRef();
+
+    // 数据处理函数
+    const [data, dataState] = useState({order: 1});
+
+    // 相当于：componentDidMount()、componentWillUpdate()
+    // 由于useEffect里面调用了dataState, 注意给其设置第二个参数[props.data, props.type, data]
+    useEffect(() => {
+        // console.log(props);
+        if(data !== props.data && props.data){
+            // 修改状态
+            dataState(props.data);
+
+            // 修改表单的数据：注意次数data是老的数据，记得用props.data来修改
+            if(props.data.id > 0 && props.type === "editor"){
+                // 需要修改一下表单的数据
+                formRef.current.setFieldsValue(props.data);
+            }
+        }
+    }, [props.data, props.type, data, formRef, props]);
+
+    const formItemLayout = {
+        labelCol: {
+            xs: { span: 24 },
+            sm: { span: 5 },
+            md: { span: 5 }
+          },
+          wrapperCol: {
+            xs: { span: 24 },
+            sm: { span: 15 },
+            md: { span: 14 }
+          }
+    };
+
+    const formItemTailLayout = {
+        wrapperCol: { offset: 8, span: 8 },
+    }
+
+    const handleOnFinish = values => {
+        if(! props.handleSubmit){
+            message.error("没传递表单提交处理函数", 5);
+            return
+        }else{
+            props.handleSubmit(values);
+        }
+    }
+
+    // 渲染表单
+    // 相当于class编写组件时的：render(){}
+    return (
+        <Form
+          ref={formRef}
+          onFinish={handleOnFinish}
+          initialValues={data}
+        >
+            <Row>
+                <Col xs={24} sm={24} md={24}>
+                    <Form.Item
+                      {...formItemLayout}
+                      label="分类名"
+                      name="name"
+                      rules={[
+                          {required: true, message: "请填写分类名"}
+                      ]}
+                    >
+                      <Input placeholder="name" />
+                    </Form.Item>
+
+                    <Form.Item
+                      {...formItemLayout}
+                      label="Code"
+                      name="code"
+                      rules={[
+                          {required: true, message: "请填写分类的code"}
+                      ]}
+                    >
+                      <Input placeholder="code" />
+                    </Form.Item>
+
+                    <Form.Item
+                      {...formItemLayout}
+                      label="父级分类"
+                      name="parent"
+                      rules={[
+                          {required: false, message: "请填写父级分类"}
+                      ]}
+                    >
+                      <Input placeholder="parent" />
+                    </Form.Item>
+
+                    <Form.Item
+                      {...formItemLayout}
+                      label="排序"
+                      name="order"
+                      rules={[
+                          {required: true, message: "请填写分类的排序"}
+                      ]}
+                    >
+                      <InputNumber min={1} max={100} />
+                    </Form.Item>
+
+                    <Form.Item
+                      {...formItemLayout}
+                      label="描述"
+                      name="description"
+                      rules={[
+                          {required: true, message: "请描述一下当前分类"}
+                      ]}
+                    >
+                      <Input.TextArea 
+                        rows={4}
+                        autoSize={{minRows: 4, maxRows: 20}}
+                        placeholder="description" />
+                    </Form.Item>
+
+                    <Form.Item
+                      style={{display: props.type === "editor" ? "show" : "none"}}
+                      {...formItemLayout}
+                      label="添加时间"
+                      rules={[
+                          {required: true, message: "请描述一下当前分类"}
+                      ]}
+                    >
+                      <Input value={data.time_added ? data.time_added : null} disabled/>
+                    </Form.Item>
+
+                    <Form.Item
+                      {...formItemLayout}
+                      label="图片"
+                      name="image"
+                      rules={[
+                          {required: false, message: "请填写图片"}
+                      ]}
+                    >
+                      <Input placeholder="parent" />
+                    </Form.Item>
+
+                    {/* 提交按钮 */}
+                    <Form.Item 
+                      style={{textAlign: "center"}}
+                      {...formItemTailLayout}>
+                         <Button type="primary" htmlType="submit">
+                             {props.type === "editor" ? "编辑" : "添加"}
+                         </Button>
+                    </Form.Item>
+                    <div>
+                        {JSON.stringify(data)}
+                    </div>
+                </Col>
+            </Row>
+        </Form>
+    );
+}
+
+export default CategoryForm;
