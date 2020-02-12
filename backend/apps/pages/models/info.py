95613694d72c30ec9219425dc0b3397edc860642
codelieche <codelieche@gmail.com>
2020-02-12 23:50:24 +0800

    add pages models

---
+++ backend/apps/pages/models/info.py
@@ -0,0 +1,120 @@
+"""
+页面属性相关的Model
+"""
+from django.db import models
+
+from pages.models.page import Page
+
+# 属性值可能是个字符，也可能是个数值，也可能是个json对象
+VALUE_TYPE_CHOICES = (
+    ("text", "文本"),
+    ("number", "数字类型"),
+    ("bool", "布尔类型"),
+    ("json", "Json对象") # 当分类中fields有多个就是json类型。
+)
+
+
+class InfoField(models.Model):
+    """字段"""
+    name = models.SlugField(verbose_name="字段名", max_length=20)
+    name_verbose = models.CharField(verbose_name="字段名(中文)", max_length=40)
+    is_active = models.BooleanField(verbose_name="有效", blank=True, default=True)
+
+    class Meta:
+        verbose_name = "字段"
+        verbose_name_plural = verbose_name
+
+
+class InfoCategory(models.Model):
+    """
+    属性的分类
+    """
+    # 元素的类型
+    ELEMENT_CHOICES = (
+        ("text", "文本"),         # 文本：默认的类型，输入字符即可
+        ("number", "数值"),       # 数值：支持整数和浮点数
+        ("select", "单选"),       # 单选
+        ("tags", "标签"),         # 多选：标签的类型，一个page可以有多个标签值
+        ("checkbox", "复选框"),   # checkbox: 选中或者不选中
+        ("date", "日期"),         # 日期有开始和一个结束
+        ("user", "用户"),         # 设置: 执行用户啊等，和tag一样是可以设置多个值的
+        ("file", "文件"),         # 文件：可以是个链接，也可以上传
+        ("image", "图片"),        # 图片：可以是个url，也可以是个文件
+        ("viedio", "视频"),       # 视频：可以是url，也可以自己上传
+        ("url", "链接"),          # 链接：点击后可以跳转到新的页面
+        ("email", "邮箱"),        # 邮箱：
+        ("phone", "电话")         # 电话：需要校验
+    )
+    name = models.CharField(verbose_name="类型名", max_length=40, unique=True)
+    icon = models.SlugField(verbose_name="分类图标", max_length="40", blank=True)
+    element = models.CharField(verbose_name="元素", choices=ELEMENT_CHOICES
+                               default="text", max_length=40, blank=True)
+    fields = models.ManyToManyField(to=InfoField, blank=True, verbose_name="信息字段")
+    order = models.SmallIntegerField(verbose_name="排序", default=1, blank=True)
+    is_active = models.BooleanField(verbose_name="有效", default=True, blank=True)
+
+    class Meta:
+        verbose_name = "属性类型"
+        verbose_name_plural = verbose_name
+
+
+class Info(models.Model):
+    """属性"""
+
+    category = models.ForeignKey(to=Category, verbose_name="分类",
+                                 blank=True, on_delete=models.CASCADE, 
+                                 default=Category.objects.first(element="text"))
+    name = models.CharField(verbose_name="属性名", blank=True, default="属性", db_index=True)
+    # 这个属性属于哪个页面
+    page = models.ForeignKey(to=Page, verbose_name="页面", related_name="infos")
+    value_type = models.CharField(verbose_name="值类型", choices=VALUE_TYPE_CHOICES, 
+                                  default="text", blank=True)
+    # 只有category__element是tags类型的值，才可以有多个值，其它每个page只能设置单值
+    is_multiple = models.BooleanField(verbose_name="是否有多值", default=False, blank=True)
+    is_active = models.BooleanField(verbose_name="有效", default=True, blank=True)
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        # 根据分类的element类型，来设置is_multiple
+        if self.category.element in ("tags", "user"):
+            self.is_multiple = True
+        else:
+            self.is_multiple = False
+        
+        # 判断值的类型：保存的方式都是字符串，但是前端需要个根据这些值来做处理
+        if self.category.element == "number":
+            self.value_type = "number"
+        elif self.category.element in ("date", ):
+            # date类型的会有两个字段：start和value
+            self.value_type = "json"
+        elif self.category.element in ("checkbox",):
+            self.value_type = "bool"
+        
+        super().save(force_insert=force_insert, force_update=force_update, 
+                     using=using, update_fields=update_fields)
+
+    class Meta:
+        verbose_name = "属性"
+        verbose_name_plural = verbose_name
+
+
+class InfoValue(models.Model):
+    """属性的值"""
+    info = models.ForeignKey(to=Info, verbose_name="属性", on_delete=models.CASCADE, 
+                             related_name="values")
+    page = models.ManyToManyField(to=Page, verbose_name="页面", on_delete=models.CASCADE)
+    value = models.CharField(verbose_name="值", max_length=512)
+    value_type = models.CharField(verbose_name="值类型", choices=VALUE_TYPE_CHOICES, default="text", blank=True)
+    order = models.PositiveSmallIntegerField(verbose_name="排序", default=1, blank=True)
+    color = models.CharField(verbose_name="颜色", max_length=40, blank=True, null=True)
+    is_active = models.BooleanField(verbose_name="有效", blank=True, default=True)
+
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        # 修改value_type
+        self.value_type = self.info.value_type
+
+        super().save(force_insert=force_insert, force_update=force_update, 
+                     using=using, update_fields=update_fields)
+        
+    class Meta:
+        verbose_name = "属性值"
+        verbose_name_plural = verbose_name
--- backend/apps/pages/models/page.py
+++ backend/apps/pages/models/page.py
@@ -3,14 +3,41 @@ Page相关的Model
 """
 from django.db import models
 
+from account.models import User
+
 
 class Page(models.Model):
     """
     Page Model
     """
+
+    CATEGORY_CHOICES = (
+        ("docs", "文档"),
+        ("kanban", "看板")
+    )
+    
     title = models.CharField(verbose_name="标题", max_length=256, 
                               blank=True, null=True, db_index=True)
-    
+    category = models.CharField(verbose_name="类型", blank=True, default="docs")
+    icon = models.CharField(verbose_name="图标", max_length=256, blank=True, null=True)
+    description = models.CharField(verbose_name="描述", max_length=1024, blank=True, null=True)
+    user = models.ForeignKey(verbose_name="创建者", blank=True, on_delete=models.SET_NULL, blank=True)
+    # 文章顶部的封面
+    cover = models.CharField(verbose_name="封面", max_length=256, blank=True, null=True)
+    # 无线内联
+    parent = models.ForeignKey(to="self", verbose_name="父页面", blank=True, 
+                               on_delete=models.CASCADE, related_name="children")
+    # 文章内容
+    content = models.TextField(verbose_name="内容", blank=True, null=True)
+
+    # 文章排序，越小的排前面
+    order = models.SmallIntegerField(verbose_name="排序", default=1, blank=True)
+    # 添加时间
+    time_added = models.DateTimeField(verbose_name="添加时间", blank=True, auto_now_add=True)
+    time_updated = models.DateTimeField(verbose_name="修改时间", blank=True, auto_now=True)
+    # 文章的内容是一系列的block
+    is_active = models.BooleanField(verbose_name="有效", blank=True, default=True)
+
 
     class Meta:
         verbose_name = "页面"
